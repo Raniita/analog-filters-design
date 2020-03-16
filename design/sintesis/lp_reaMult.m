5cd6b634d89cae11d5b8c264fb2dcf9d838fa11e
Raniita <efdezsanchez@gmail.com>
2020-03-16 01:56:12 +0100

    working on sintesis

--- design/sintesis/lp_reaMult.m
+++ design/sintesis/lp_reaMult.m
@@ -2,10 +2,10 @@ function [R1, C2, R3, R4, C5] = lp_reaMult (Ho, alpha, omega, ref)
   C5 = ref;
   
   k = (4*(Ho+1))/(alpha)^2;
-  C2 = k*C5;
+  C2 = 10*round(k)*C5;
   
-  raiz = sqrt(1-((4*(Ho+1))/(k*alpha^2)));
-  R4 = (alpha/(2*omega*C5))*(1 + raiz); % Revisar, aqui falta +-
+  raiz = sqrt(1-(k*2*k^(-1)));
+  R4 = (alpha/(2*omega*C5))*(1 - raiz); % Revisar, aqui falta +-
   R1 = R4/Ho;
-  R3 = (1)/((omega^2)*(C5^2)*R4*k);
+  R3 = ((omega^2)*(C5^2)*R4*2*k)^-1;
 end
